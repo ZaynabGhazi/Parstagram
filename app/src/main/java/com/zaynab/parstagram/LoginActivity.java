5b8fe5e73976697d76a51aa925c49540f68c9280
Zaynab Ghazi <zaynabb@fb.com>
2020-07-06 15:50:14 -0400

    added new rule to .gitignore + implemented sign up

--- app/src/main/java/com/zaynab/parstagram/LoginActivity.java
+++ app/src/main/java/com/zaynab/parstagram/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,17 +21,19 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        //user persistence
         if (ParseUser.getCurrentUser() != null) goMainActivity();
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +44,36 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username, password);
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"SignUp button clicked");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username, password);
+            }
+        });
+    }
+
+    private void signupUser(String username, String password) {
+        Log.i(TAG,"Attempt to signup user "+username);
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                   goMainActivity();
+                    Toast.makeText(LoginActivity.this,"Sign-up successful!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.e(TAG,"Issue with sign-up.",e);
+                    Toast.makeText(LoginActivity.this, "Username or password does not match criteria.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
     }
 
     private void loginUser(String username, String password) {
